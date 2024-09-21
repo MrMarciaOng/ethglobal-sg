3e2d63160806acea46728b3eddb1d79af4c67295
hougangdev <devchain7890@gmail.com>
2024-09-21 17:18:11 +0800

    add MockUsdt

---
+++ smart-contract/src/MockUsdt.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.18;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockUsdt is ERC20{
+    uint8 private _decimals;
+
+    constructor(string memory name, string memory symbol, uint8 decimalsValue) ERC20(name, symbol) {
+        _decimals = decimalsValue;
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+
+    function decimals() public view virtual override returns (uint8) {
+        return _decimals;
+    }
+}
